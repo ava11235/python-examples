f1f88ee89a223d1dfcc13637d02e145683c0452c
ava11235 <stani.instructor@gmail.com>
2019-06-23 14:46:25 -0700

    Add files via upload

---
+++ pythonbasics.py
@@ -0,0 +1,84 @@
+
+
+print("Hello, World!")
+print(42 + 7)
+
+print("42 + 7 = ", 24 + 7)
+
+def hi():
+	print("Hi")
+	print("Have a nice afternoon!")
+
+	
+hi()
+
+def hello(personName):
+	print("Hi", personName)
+	print("How are you today?")
+
+	
+hello("AJ")
+
+
+hello("Alicia")
+
+print()
+
+#this will cause an error
+hello
+
+print
+<built-in function print>
+>>> 
+
+
+
+#hello.py
+def main():
+    #get user input 
+    personName = input("Enter your name ")    
+    print("Hello,",personName,"How are you?")
+
+
+main()
+
+
+#hello1.py
+
+def hello(personName):
+    print("Hello,",personName,"How are you?")
+
+def main():
+    #get user input 
+    person = input("Enter your name ")
+    greet(personName)
+
+main()
+
+
+#range function examples
+for i in range(7):
+	print(i)
+
+	
+for i in range(2, 7):
+	print(i)
+
+	
+
+
+for odd in range(1, 10, 2):
+	print(odd)
+
+	
+
+
+for even in range(2, 11, 2):
+	print(even)
+
+	
+
+
+
+
+
