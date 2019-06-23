0e113dfa3edf4f92cea002f1a9d875539db0e0b5
ava11235 <stani.instructor@gmail.com>
2019-06-23 15:13:54 -0700

    Add files via upload

---
+++ python-basics2.py
@@ -0,0 +1,58 @@
+name = input("Enter your name: ")
+# Enter your name: Ava
+name
+#'Ava'
+>>> 
+
+#swap two values
+x = 7
+y = 9
+temp = x
+x = y
+y = temp
+x
+y
+
+
+#swap two values simultaneous assignment
+x = 7
+y = 9
+x, y = y, x
+x
+y
+
+
+#definite loops
+for i in [0, 1, 2, 3, 4, 5]:
+	print(i)
+
+	
+
+for i in [0, 1, 2, 3, 4, 5]:
+	print(i, end=" ")
+
+	
+
+
+for odd in [1, 3, 5, 7, 9]:
+	print(odd)
+
+	
+
+
+for odd in [1, 3, 5, 7, 9]:
+	print(odd * odd)
+
+	
+
+for i in range(10):
+	print(i)
+
+	
+
+#turn range(10) into a list
+list(range(10))
+
+
+
+
