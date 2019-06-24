f0dcfc591068362ed450ac74f9955139a00b3435
ava11235 <stani.instructor@gmail.com>
2019-06-23 17:29:49 -0700

    Add files via upload

---
+++ temp-converter.py
@@ -0,0 +1,8 @@
+#convert Celsius temps to Fahrenheit
+
+def main():
+    celsius = eval(input("Enter the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The Fahrenheit temperature is", fahrenheit)
+
+main()
