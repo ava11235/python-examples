2cb60b855fb2e7247166bb547bc34d434222e1d4
ava11235 <stani.instructor@gmail.com>
2019-06-23 17:17:26 -0700

    Add files via upload

---
+++ average.py
@@ -0,0 +1,12 @@
+#compute the average of two numbers
+
+def main():
+    print("Let's compute the average of two numbers")
+
+  num1, num2 = eval(input("Enter first number, then second number separated separated by a comma: "))
+    average = (num1 + numb2) / 2
+
+    print("The average of", num1, "and", num2", is:", average)
+
+main()
+
