c9a5bb6b2b3a0c45dbbf42abcd40d76c41a4a1a8
ava11235 <stani.instructor@gmail.com>
2019-07-03 11:45:04 -0700

    Add files via upload

---
+++ work-with-mathlib.py
@@ -0,0 +1,20 @@
+import math as m
+
+#power and square root
+
+m.pow(2,3) #8
+m.sqrt(16) #4
+
+#circle
+radius = 12
+circumference = m.pi* radius * 2
+area = m.pi * radius ** 2
+
+#floor (round down, towards floor), ceiling(round up, towards ceiling)
+m.floor(15.546) #15
+m.ceil(15.546) #16
+m.floor(-4.443) #-5
+m.ceil(-4.443) #-4
+
+    
+    
