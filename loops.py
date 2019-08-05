5b2ee37686c94e866f5dd0a28e90a4c372cba4d3
ava11235 <stani.instructor@gmail.com>
2019-08-04 20:31:02 -0700

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,51 @@
+#loops
+>>> sum = 0
+>>> i = 1
+>>> n = 5
+>>> while i <= n:
+	sum = sum + i
+	i = i + 1
+
+	
+>>> i
+6
+>>> sum
+15
+>>> sum = 0
+>>> i = 1
+>>> while i < 2 * n:
+	sum = sum + 1
+	i = i + 2
+
+	
+>>> sum
+5
+>>> sum = 0
+>>> x = int(input("Enter a number. 999 to exit "))
+Enter a number. 999 to exit 3
+>>> while x != 999:
+	sum = sum + x
+	x = int(input("Enter a number. 999 to exit "))
+
+	
+Enter a number. 999 to exit 1
+Enter a number. 999 to exit 2
+Enter a number. 999 to exit 3
+Enter a number. 999 to exit 999
+>>> sum
+9
+>>> count = 0
+>>> n = 4
+>>> while n > 1:
+	n = n // 2
+	count = count + 1
+
+	
+>>> n
+1.0
+>>> count
+2
+>>> 
+
+
+
